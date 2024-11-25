eb0521fc2c34348c413af6b0bb6b6eb9a23c2ede
Sumsan38 <ndanl4647@gmail.com>
2024-11-25 09:20:57 +0900

    재료양 관련 변경

--- services/save_recipe_ingredients.py
+++ services/save_recipe_ingredients.py
@@ -5,9 +5,11 @@ def save_recipe_ingredients(session, recipe_id, ingredients):
     """Save recipe ingredients into bob_recipe_ingredients table."""
     for ingredient_data in ingredients:
         ingredient_id = save_ingredient(session, ingredient_data["name"])
+        volume_ = ingredient_data["volume"]
         recipe_ingredient = BobRecipeIngredient(
             recipe_id=recipe_id,
             ingredient_id=ingredient_id,
+            amount=volume_,
             reg_id="system@system.com"
         )
         session.add(recipe_ingredient)
